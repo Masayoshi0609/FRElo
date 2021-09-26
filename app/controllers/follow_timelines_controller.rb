83bbe92a4e0ca2a5e5151e8bfe931ac946880fe3
Masayoshi0609 <masayoshi9876@gmail.com>
2021-09-26 06:33:57 +0000

    フォロータイムライン完成

--- app/controllers/follow_timelines_controller.rb
+++ app/controllers/follow_timelines_controller.rb
@@ -1,4 +1,19 @@
 class FollowTimelinesController < ApplicationController
   def show
+    @user = current_user
+    @post = Post.new
+    @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).reverse_order.page(params[:page]).per(3)
+    @tag_list = Tag.all.reverse_order.page(params[:page]).per(7)
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :telephone_number, :is_deleted, :height, :weight, :age, :living_area, :body_type, :exercise_frequency, :fat_percentage, :introduction)
+  end
+
+  def post_params
+    params.require(:post).permit(:body, :image, :user_id)
+  end
+
 end
