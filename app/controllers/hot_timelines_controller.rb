e0f828acc906a244237699f1835820c06e996d0c
Masayoshi0609 <masayoshi9876@gmail.com>
2021-09-28 08:43:27 +0000

    全体タイムライン追加

--- app/controllers/hot_timelines_controller.rb
+++ app/controllers/hot_timelines_controller.rb
@@ -1,4 +1,19 @@
 class HotTimelinesController < ApplicationController
   def show
+    @user = current_user
+    @post = Post.new
+    @posts = Post.all.reverse_order.page(params[:page]).per(10)
+    @tag_list = Tag.includes(:post_tags, :posts).order("posts.created_at DESC").page(params[:page]).per(7)
+  end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :telephone_number, :is_deleted, :height, :weight, :age, :living_area, :body_type, :exercise_frequency, :fat_percentage, :introduction)
+  end
+
+  def post_params
+    params.require(:post).permit(:body, :image, :user_id)
   end
 end
